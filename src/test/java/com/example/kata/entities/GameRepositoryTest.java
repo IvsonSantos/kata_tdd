7b39019faf7f2e0667b4756b5a30c4175b311a26
ivsonsantos <Belgique@2024>
2021-06-15 17:12:14 +0200

    TDD - Test for Create Game

---
+++ src/test/java/com/example/kata/entities/GameRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.kata.entities;
+
+import com.example.kata.entity.Game;
+import com.example.kata.entity.Player;
+import com.example.kata.repository.GameRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class GameRepositoryTest {
+	
+	@Autowired
+    private TestEntityManager entityManager;
+     
+    @Autowired
+    private GameRepository repository;
+     
+    @Test
+    public void testCreateAndSaveGameWithId1() {
+
+        entityManager.persist(new Game());
+
+        Optional<Game> gameOptional = repository.findById(1);
+
+        assertThat(gameOptional).isNotEmpty();
+    }
+
+}
